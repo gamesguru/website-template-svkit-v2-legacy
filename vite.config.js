0615a1281c0415a3f7d972058693d53b495890b2
Shane Jaroch <chown_tee@proton.me>
2026-01-15 04:10:37 -0500

    update name

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@tg-svelte/kit/vite';
 import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
